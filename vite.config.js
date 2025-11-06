99725b288d253907326aa0e6374b8536e8cec3ac
Pravintobi <a.pravin1922@gmail.com>
2025-11-06 12:19:38 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import commonjs from 'vite-plugin-commonjs';
-
-export default defineConfig({
-  plugins: [commonjs()],
-  optimizeDeps: {
-    include: ['node-vibrant']
-  }
-});
